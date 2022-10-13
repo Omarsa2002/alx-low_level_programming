4ee1b2d1a15fb4f23327367434f442a5ae8d2ec3
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-13 23:20:55 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,33 +1,11 @@
 #include<stdio.h>
-#define ll long long
-// //#define p(n,os) pow(n,os)
-// #define dd long double
-// #define des(x1,y1,x2,y2) sqrt(pow(x1-x2,2)+pow(y1-y2,2))
-// #define nl '\n'
-// #define PI acos(-1)
-// #define pi 3.14159265
-// #define cin(vec) for(auto& i : vec) cin >> i;
-// #define cout(vec) for(auto i : vec) cout << i<<' ' ; cout << "\n";
-// #define hole(x) x.begin(),x.end()
-// #define Mabrouk ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-void test_case(ll t)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    //printf("%d",1);
-    puts("\"Programming is like building a multilingual puzzle");
-}
-int main()
-{   
-    //sieve_algorithm();
-    //freopen("strings.in","r",stdin);
-    //Mabrouk_Ata();
-    //Mabrouk;
-    //cout<<fixed<<setprecision(0);
-    ll t=1;
-    //cin>>t;
-    for(int i=1;i<=t;i++)
-    {
-        test_case(t);
-        //cout<<nl;
-    }
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
