a0a6f4a1deaa1fe42616a1eaeb2e9d3c82b5beae
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-18 22:22:34 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,12 +12,9 @@ int main(void)
 {
 	char a[] = "_putchar";
 	int i = 0;
-	int size = sizeof(a)-1;
-	
-	for(i = 0 ; i < size ; i++)
+	int size = sizeof(a) - 1;	
+	for (i = 0 ; i < size ; i++)
 		_putchar(a[i]);
-	
 	_putchar('\n');
-	
 	return (0);
 }
