831fa515b91d232f1fe25257b4d9c8fa59722f00
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-19 01:46:55 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - prints the type
+ *of the number
+ *@n: an integer char
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for( ;n >= 98; n--)
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+	}
+	else
+	{
+		for( ;n <= 98; n++)
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+	}
+}
