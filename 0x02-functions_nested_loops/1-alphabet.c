650eaefa85f4956b213933bcd06cf848427083f3
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-18 22:45:00 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
