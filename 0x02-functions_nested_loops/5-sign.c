df1345c8855aa77ca926add1609f967081c665b3
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-19 00:10:45 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_sign - prints the type
+ *of the number
+ *@n: an integer char
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if(n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
