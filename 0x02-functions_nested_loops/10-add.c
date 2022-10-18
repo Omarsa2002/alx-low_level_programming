5cbba163740380d7c39c557e90f2bc842e6b1077
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-19 01:36:53 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *add - prints the type
+ *of the number
+ *@a: an integer char
+ *@b: an integer
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
