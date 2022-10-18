2039055aaff02997493dd8f43edf5f782559d293
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-19 01:32:27 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,20 +11,42 @@
  */
 void times_table(void)
 {
-	int i=0;
-	int j=0;
+	char tr = ' ';
+	int i;
+	int j;
+	int k;
+	int l;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		for (j = 0; j < 10; j++)
 		{
+			k = i * j;
 			if (j != 0)
-				_putchar(' ');
-			_putchar(i * j);
+			{
+				_putchar(tr);
+				if (k < 10)
+				{
+					_putchar(tr);
+					_putchar(k + '0');
+				}
+				else
+				{
+					l = k % 10;
+					k = k / 10;
+					_putchar(k + '0');
+					_putchar(l + '0');
+				}
+			}
+			else
+			{
+				_putchar(k + '0');
+			}
 			if (j != 9)
 				_putchar(',');
 			else
 				_putchar('\n');
 		}
 	}
+}
 }
