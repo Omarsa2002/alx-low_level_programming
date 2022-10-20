1ba83e11cb3130cc7bba381369e382a067073384
Omar_mabrouk <89041994+Omarsa2002@users.noreply.github.com>
2022-10-20 23:02:22 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,14 @@
 #include "main.h"
 
 /**
- *_isupper - check a char
- *@c: variable
- *
- *Return: 0 or 2
- */
+*_isupper - check a char
+*@c: variable
+*
+*Return: 0 or 2
+*/
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
 }
